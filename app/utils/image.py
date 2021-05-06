6390362353f89b8ee32d09eb10526c501d9f0c36
Kienyew <aobi100bt@hotmail.com>
2021-05-06 12:44:19 +0800

    Fix image compress function

--- app/utils/image.py
+++ app/utils/image.py
@@ -6,5 +6,5 @@ from PIL import Image
 def compress(image_data: bytes) -> bytes:
     output = io.BytesIO()
     image = Image.open(io.BytesIO(image_data))
-    image.save(output, image.format)
+    image.save(output, image.format, optimize=True, quality=95)
     return output.getvalue()
